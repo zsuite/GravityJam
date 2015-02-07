8a3e20c959034e539a846a858b4b3f44d5a2cfda
Zach Suite <zachissuite@gmail.com>
2015-02-07 12:06:47 -0800

    Initial Commit

---
+++ Assets/Scripts/MoveTowardsBody.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsBody : MonoBehaviour {
+	public Transform target;
+	public float gravityNum = 9.8f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 gravityDirection = transform.position - target.position;
+		target.rigidbody.AddForce(gravityDirection.normalized * gravityNum * Time.fixedDeltaTime);
+	}
+}
